887d94134aff3d7ea465566e155484216d31bdbf
Mark R Ohnsman <MarkOhnsman@users.noreply.github.com>
2017-11-14 08:47:08 -0700

    broke it

--- app/slap/slap-service.js
+++ app/slap/slap-service.js
@@ -5,18 +5,18 @@
 function SlapService() {
     // PRIVATE PARTS
     //SERVICE private
-    var target = new Target("Scarecrow", 100, 1, 5, 10, 200);
+    var target = Target("Scarecrow", 100, 1, 5, 10)
     //Service private
     var items = {
         shield: new Item('Shield', -0.3, "This is a way cool shield"),
         crows: new Item("Crows", 0.5, "More CROWS!"),
         straw: new Item("Straw", -0.2, "Stronger straw"),
         fire: new Item("Fire", 1.6, "its super effective")
-    };
+    }
     //DATA service private
     function Target(name, health, slap, punch, kick, roundhouse) {
         this.name = name;
-        this.health = health;
+        this.health = helth;
         this.attacks = {
             "slap": slap,
             "punch": punch,
@@ -25,14 +25,12 @@ function SlapService() {
         };
         this.items = []
         this.hits = 0
-    };
-    //service private
+    }
     function Item(name, modifier, description) {
         this.name = name;
         this.modifier = modifier;
         this.description = description;
-    };
-    //Service private
+    
     function addMods() {
         var total = 1;
         for (var index = 0; index < target.items.length; index++) {
@@ -40,11 +38,10 @@ function SlapService() {
             total += item.modifier
         }
         return total
-    };
+        }
+    }
 
-    //  PUBLIC == this 
-
-    this.attack = function attack(type) {
+    function attack(type) {
         //DO STUFF
         if (target.attacks[type]) {
             target.health -= target.attacks[type] * addMods()
@@ -53,7 +50,7 @@ function SlapService() {
     }
 
     this.getTarget = function getTarget() {
-        return JSON.parse(JSON.stringify(target)) // non-primative passed by reference
+        JSON.parse(JSON.stringify(target)) // non-primative passed by reference
         // primatives are passed by value
     }
 
